098af46b79fd155e0ac35647281fad8b68ea1541
kusa raju <21pa1a05e9@vishnu.edu.in>
2024-02-23 19:06:57 +0530

    User model created

---
+++ Server/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    id : {
+        type : Number,
+        unique : true,
+        required : true
+    },
+    phone_number : {
+        type : Number,
+        required : true
+    },
+    priority : {
+        type : Number,
+        enum : [0,1,2],
+        default : 0
+    }
+})
+
+const User = mongoose.model('user',UserSchema)
+module.exports = User
